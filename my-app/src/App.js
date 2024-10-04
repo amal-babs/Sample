f6def5406d48c8cbbd6db872811fb0609f91ed1f
ARDRA MOHANLAL <116721296+ArdraMohanlal@users.noreply.github.com>
2024-10-04 22:58:15 +0530

    Chabgeed Develop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sample from './components/Sample';
+import Sim from './components/Sim';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       
-      <Route path="/" element={<Sample/>}/>
+      <Route path="/" element={<Sim/>}/>
+
       
 
     </Routes>
