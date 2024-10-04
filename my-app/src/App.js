6cfcf655fd533b7fb58c56b0c4c9618b7143f02b
Amal V B <sng22mca16@sngce.ac.in>
2024-10-04 22:45:29 +0530

    1st commit
    ardra

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sample from './components/Sample';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      
+      <Route path="/" element={<Sample/>}/>
+      
+
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
